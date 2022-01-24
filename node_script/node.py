a3aa6d7a0d3f2e22db28779d3fd03690c26054af
HiroIshida <spitfire.docomo@gmail.com>
2022-01-25 03:01:26 +0900

    Tmp

--- node_script/node.py
+++ node_script/node.py
@@ -64,12 +64,12 @@ class DeticRosNode:
         # As for large buff_size please see:
         # https://answers.ros.org/question/220502/image-subscriber-lag-despite-queue-1/?answer=220505?answer=220505#post-id-22050://answers.ros.org/question/220502/image-subscriber-lag-despite-queue-1/?answer=220505?answer=220505#post-id-220505
         self.sub = rospy.Subscriber('~input_image', Image, self.callback, queue_size=1, buff_size=2**24)
-        self.pub_debug_image = rospy.Publisher('~debug_image', Image, queue_size=10)
-        self.pub_segmentation_image = rospy.Publisher('~segmentation_image', Image, queue_size=10)
+        self.pub_debug_image = rospy.Publisher('~debug_image', Image, queue_size=1000)
+        self.pub_segmentation_image = rospy.Publisher('~segmentation_image', Image, queue_size=1000)
         if node_config.out_debug_segimage:
-            self.pub_debug_segmentation_image = rospy.Publisher('~debug_segmentation_image', Image, queue_size=10)
+            self.pub_debug_segmentation_image = rospy.Publisher('~debug_segmentation_image', Image, queue_size=1000)
 
-        self.pub_info = rospy.Publisher('~segmentation_info', SegmentationInfo, tcp_nodelay=True, queue_size=10)
+        self.pub_info = rospy.Publisher('~segmentation_info', SegmentationInfo, tcp_nodelay=True, queue_size=1000)
         rospy.loginfo('initialized node')
 
     def callback(self, msg: Image):
