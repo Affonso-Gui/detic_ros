07c9ecc7605807a08e13c3ffdd8a5bdd1fc781d9
Guilherme Affonso <affonso@jsk.imi.i.u-tokyo.ac.jp>
2023-01-03 09:58:17 +0000

    Add output/raw/centroid without point cloud size filtering

--- node_script/filter_posearray.py
+++ node_script/filter_posearray.py
@@ -4,21 +4,15 @@ import rospy
 from geometry_msgs.msg import PoseArray
 
 class FilterEmpty:
-    def __init__(self, input_topic, output_topic, output_topic_table):
+    def __init__(self, input_topic, output_topic):
         self.sub = rospy.Subscriber(input_topic, PoseArray, self.republish)
         self.pub = rospy.Publisher(output_topic, PoseArray, queue_size=1)
-        self.pub_table = rospy.Publisher(output_topic_table, PoseArray, queue_size=1)
 
     def republish(self, msg):
+        msg.poses = [x for x in msg.poses if self.check_pose(x)]
         if msg.poses:
             msg.poses.sort(key=lambda p: p.position.y)
             self.pub.publish(msg)
-            self.republish_table(msg)
-
-    def republish_table(self, msg):
-        msg.poses = [x for x in msg.poses if self.check_pose(x)]
-        if msg.poses:
-            self.pub_table.publish(msg)
 
     def check_pose(self, pose):
         return (0.070 < pose.position.z < 0.73) and (pose.position.x < 0.50)
