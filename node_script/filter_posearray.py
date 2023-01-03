cef950ec995bbd0a2999a5c4906f7a863e262a1a
Guilherme Affonso <affonso@jsk.imi.i.u-tokyo.ac.jp>
2023-01-03 03:28:55 +0000

    Filter table cards and empty messages

--- node_script/filter_posearray.py
+++ node_script/filter_posearray.py
@@ -4,13 +4,24 @@ import rospy
 from geometry_msgs.msg import PoseArray
 
 class FilterEmpty:
-    def __init__(self, input_topic, output_topic):
+    def __init__(self, input_topic, output_topic, output_topic_table):
         self.sub = rospy.Subscriber(input_topic, PoseArray, self.republish)
         self.pub = rospy.Publisher(output_topic, PoseArray, queue_size=1)
+        self.pub_table = rospy.Publisher(output_topic_table, PoseArray, queue_size=1)
 
     def republish(self, msg):
         if msg.poses:
             self.pub.publish(msg)
+            self.republish_table(msg)
+
+    def republish_table(self, msg):
+        msg.poses = [x for x in msg.poses if self.check_pose(x)]
+        if msg.poses:
+            self.pub_table.publish(msg)
+
+    def check_pose(self, pose):
+        return (0.070 < pose.position.z < 0.73)
+
 
 if __name__ == '__main__':
     rospy.init_node('filter_pose_array', anonymous=True)
