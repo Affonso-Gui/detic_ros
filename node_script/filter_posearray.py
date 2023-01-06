d92f87f83e681a05463f3eede9e3b020381ba681
Guilherme Affonso <guilherme.c.affonso@gmail.com>
2023-01-06 16:29:27 +0900

    Also filter ClusterPointIndices in filter_posearray.py

--- node_script/filter_posearray.py
+++ node_script/filter_posearray.py
@@ -2,17 +2,35 @@
 import sys
 import rospy
 from geometry_msgs.msg import PoseArray
+from jsk_recognition_msgs.msg import ClusterPointIndices
 
-class FilterEmpty:
-    def __init__(self, input_topic, output_topic):
+# Filter empty poses and apply ROI
+class FilterPoses:
+    def __init__(self, input_topic, output_topic, input_indices_topic, output_indices_topic):
         self.sub = rospy.Subscriber(input_topic, PoseArray, self.republish)
         self.pub = rospy.Publisher(output_topic, PoseArray, queue_size=1)
+        self.indices_sub = rospy.Subscriber(input_indices_topic, ClusterPointIndices, self.update)
+        self.indices_pub = rospy.Publisher(output_indices_topic, ClusterPointIndices, queue_size=1)
+        self.indices = None
+
+    def update(self, msg):
+        self.indices = msg
 
     def republish(self, msg):
-        msg.poses = [x for x in msg.poses if self.check_pose(x)]
-        if msg.poses:
-            msg.poses.sort(key=lambda p: p.position.y)
+        poses = [x for x in msg.poses if self.check_pose(x)]
+        if poses:
+            poses.sort(key=lambda p: p.position.y)
+            idx = msg.poses.index(poses[0])
+            msg.poses = poses
             self.pub.publish(msg)
+            self.republish_indices(idx, msg.header)
+
+    def republish_indices(self, idx, header=None):
+        if self.indices:
+            self.indices.cluster_indices = [self.indices.cluster_indices[idx]]
+            if header:
+                self.indices.header=header
+            self.indices_pub.publish(self.indices)
 
     def check_pose(self, pose):
         return (0.070 < pose.position.z < 0.73) and (pose.position.x < 0.50)
@@ -21,5 +39,5 @@ class FilterEmpty:
 if __name__ == '__main__':
     rospy.init_node('filter_pose_array', anonymous=True)
     argv = rospy.myargv(argv=sys.argv)
-    FilterEmpty(*argv[1:])
+    FilterPoses(*argv[1:])
     rospy.spin()
